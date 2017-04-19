0e38ac7d10c5ac3d9f504f7ee82bb6683b82992f
Martin Mroz <martinmroz@gmail.com>
2017-04-18 23:16:20 -0400

    Integration tests for all public methods, without checking the actual data sent, just its length.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -101,3 +101,117 @@ fn test_init() {
   assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(40));
   assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
 }
+
+#[test]
+fn test_clear() {
+  let (ref mut control, ref mut data, ref log) = create_test_setup();
+  let mut display = ssd1325::Ssd1325::new(data, control);
+
+  // Perform the clear sequence.
+  display.clear().unwrap();
+  
+  // Expected initialization flow:
+  //  - Enter Command.
+  //  - Send Data (Clear Sequence).
+  //  - Enter Idle.
+  let event_log = log.borrow_mut();
+  assert_eq!(event_log.len(), 3);
+
+  let mut event_log_iter = event_log.iter();
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(6));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+}
+
+#[test]
+fn test_set_on_off() {
+  let (ref mut control, ref mut data, ref log) = create_test_setup();
+  let mut display = ssd1325::Ssd1325::new(data, control);
+
+  // Perform the on/off sequence.
+  display.set_on(true).unwrap();
+  display.set_on(false).unwrap();
+  
+  // Expected display on/off flow:
+  //  - Enter Command.
+  //  - Send Data (On).
+  //  - Enter Idle.
+  //  - Enter Command.
+  //  - Send Data (Off).
+  //  - Enter Idle.
+  let event_log = log.borrow_mut();
+  assert_eq!(event_log.len(), 6);
+
+  let mut event_log_iter = event_log.iter();
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(1));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(1));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+}
+
+#[test]
+fn test_set_inverted_normal() {
+  let (ref mut control, ref mut data, ref log) = create_test_setup();
+  let mut display = ssd1325::Ssd1325::new(data, control);
+
+  // Perform the invert/normal sequence.
+  display.set_inverted(true).unwrap();
+  display.set_inverted(false).unwrap();
+  
+  // Expected invert on/off flow:
+  //  - Enter Command.
+  //  - Send Data (On).
+  //  - Enter Idle.
+  //  - Enter Command.
+  //  - Send Data (Off).
+  //  - Enter Idle.
+  let event_log = log.borrow_mut();
+  assert_eq!(event_log.len(), 6);
+
+  let mut event_log_iter = event_log.iter();
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(1));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(1));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+}
+
+#[test]
+fn test_blit_l1() {
+  let (ref mut control, ref mut data, ref log) = create_test_setup();
+  let mut display = ssd1325::Ssd1325::new(data, control);
+
+  // Build an all-on image test sequence to blit.
+  let test_sequence = &[[0xFFu8; 16]; 64];
+
+  // Blit the image to the screen.
+  display.blit_l1(test_sequence).unwrap();
+
+  // Expected initialization flow:
+  //  - Enter Command.
+  //  - Send Data (6).
+  //  - Enter Idle.
+  // [ 64x
+  //    - Enter Data.
+  //    - Send Data (64).
+  //    - Enter Idle.
+  // ]
+  let event_log = log.borrow_mut();
+  assert_eq!(event_log.len(), 3 + (64 * 3));
+
+  // Check the blit preamble was sent.
+  let mut event_log_iter = event_log.iter();
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterCommand);
+  assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(6));
+  assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+
+  // Check all 64 lines were sent.
+  for _ in 0 .. 64 {
+    assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterData);
+    assert_eq!(event_log_iter.next().unwrap(), &Event::SendData(64));
+    assert_eq!(event_log_iter.next().unwrap(), &Event::ControlChannelEnterIdle);
+  }
+}
